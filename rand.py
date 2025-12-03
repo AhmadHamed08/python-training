7fded7ba1280a21ac9b5b84f690757fcaf67a2c2
AhmadHamed08 <ahmad.omar.hamed@gmail.com>
2025-12-03 10:48:33 +0100

    rand training

---
+++ rand.py
@@ -0,0 +1,69 @@
+import random
+
+# tal = random.randint(1, 100)
+# while True:
+#     gissning = int(input("gissa talet 1-100: "))
+#     if gissning > tal:
+#         print("för högt")
+#     elif gissning < tal:
+#         print("för lågt")
+#     else:
+#         print("rätt")
+#         break
+
+
+# namn = ["alex", "jake", "albert", "clark"]
+# vald_namn = ""
+# while True:
+#     val = input("Välja mellan tärning, tärning2 och namn. 0 för att avsluta: ")
+#     if val == "tärning":
+#         print(random.randint(1, 6))
+#     elif val == "tärning2":
+#         print(random.randint(1, 6))
+#         print(random.randint(1, 6))
+#     elif val == "namn":
+#         if len(namn) == 0:
+#             print("namn lista är tomt")
+#             continue
+#         vald_namn = random.choice(namn)
+#         print(vald_namn)
+#         namn.remove(vald_namn)
+#     elif val == "0":
+#         break
+
+
+# antal_försök = 7
+# tal = random.randint(1, 100)
+# while True:
+#     gissning = int(input("gissa talet 1-100: "))
+#     if gissning > tal:
+#         print("för högt")
+#     elif gissning < tal:
+#         print("för lågt")
+#     else:
+#         print("rätt")
+#         spela_igen = input("Vill du köra igen? y/n")
+#         if spela_igen == "y":
+#             antal_försök = 7
+#             tal = random.randint(1, 100)
+#             continue
+#         else:
+#             break
+    
+#     antal_försök -= 1
+#     print(f"antal försök kvar: {antal_försök}")
+#     if antal_försök == 0:
+#         spela_igen = input("Vill du köra igen? y/n")
+#         if spela_igen == "y":
+#             antal_försök = 7
+#             tal = random.randint(1, 100)
+#             continue
+#         else:
+#             break
+
+
+
+
+
+    
+
